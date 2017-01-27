aa17370464341b3abb477d483cf8627a0038ffed
App Academy Student <student@appacademy.io>
2017-01-26 17:39:59 -0800

    add display and square logic for icons

--- board.rb
+++ board.rb
@@ -1,17 +1,17 @@
 require_relative 'square'
 require 'byebug'
 class Board
-  attr_reader :grid
+  attr_reader :grid, :size
 
   def initialize
     @grid = Array.new(9){Array.new(9) { Square.new } }
+    @size = 9
   end
 
   def seed_bombs
     count = 0
     until count == 12
       new_pos = rand_pos
-      debugger
       unless self[new_pos].identity == :bomb
         place_bomb(new_pos)
         count += 1
@@ -37,8 +37,46 @@ class Board
     @grid[x][y] = value
   end
 
-  def neighbors(square)
+  def process_neighbors
+    i = 0
+    while i < @size
+      j = 0
+      while j < @size
+        self[[i,j]].num_nearby = find_nearby([i,j]) # sets num_nearby to the number from find_nearby
+        j += 1
+      end
+      i +=1
+    end
   end
 
+  def find_nearby(pos)
+    count = 0
+    bombs = []
+    neighbor_positions(pos).each do |sur_pos|
+      count += 1 if self[sur_pos].identity == :bomb
+    end
+    count
+  end
 
+  def neighbor_positions(pos)
+    x,y = pos
+    above_row = [[x-1,y-1],[x,y-1],[x+1,y-1]]
+    same_row = [[x-1,y],[x+1,y]]
+    below_row = [[x-1,y+1],[x,y+1],[x+1,y+1]]
+
+    all_squares = above_row.concat(same_row.concat(below_row))
+
+    all_squares = all_squares.select { |pos| pos[0].between?(0,@size-1) }
+    all_squares.select { |pos| pos[1].between?(0,@size-1) }
+
+  end
+
+  def render
+    grid.each do |row|
+      print "\n"
+      row.each do |el|
+        print el.display
+      end
+    end
+  end
 end
--- square.rb
+++ square.rb
@@ -1,24 +1,39 @@
 class Square
-  attr_accessor :identity
+  attr_accessor :identity, :num_nearby, :revealed
 
   def initialize
     @identity = :empty
+    @num_nearby = nil
+    @revealed = false
+    @current_icon = "|_|"
   end
 
   def has_bomb?
   end
 
   def reveal
+    @current_icon = " * " if self.identity == :bomb
+    @current_icon = " P " if self.identity == :flag
+    @current_icon = " #{num_nearby} " if self.identity == :empty
+    @revealed = true
   end
 
   def hide
   end
 
-  def flag
+  def flag_me
+    @identity = :flag
   end
 
   def position
   end
+
+  def display
+    @current_icon
+  end
+
+
+
 
 
 
